d9541f11c32aed4ceb90590026e4a8887b751f36
webakos <akos.czimbalmos@webapix.hu>
2026-06-25 19:33:56 +0300

    init

---
+++ src/Entity/Review.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReviewRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReviewRepository::class)]
+class Review
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $companyName = null;
+
+    #[ORM\Column]
+    private ?int $rating = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $reviewText = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $authorEmail = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCompanyName(): ?string
+    {
+        return $this->companyName;
+    }
+
+    public function setCompanyName(string $companyName): static
+    {
+        $this->companyName = $companyName;
+
+        return $this;
+    }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(int $rating): static
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    public function getReviewText(): ?string
+    {
+        return $this->reviewText;
+    }
+
+    public function setReviewText(string $reviewText): static
+    {
+        $this->reviewText = $reviewText;
+
+        return $this;
+    }
+
+    public function getAuthorEmail(): ?string
+    {
+        return $this->authorEmail;
+    }
+
+    public function setAuthorEmail(string $authorEmail): static
+    {
+        $this->authorEmail = $authorEmail;
+
+        return $this;
+    }
+}
